2e73d873db68589caa262079ea14e42ad2a18fc2
wuqifei <wuqifei001@qq.com>
2019-07-04 16:28:18 +0800

    音乐分类接口返回图片

--- server/map/musicSqlMap.js
+++ server/map/musicSqlMap.js
@@ -17,9 +17,9 @@ let musicSqlMap = {
     music_click_number: 'update music set click_number = (select click_number from (select music_name,singer,' +
       'click_number from music)t where t.music_name = ? and t.singer = ?) + 1 where music_name = ? and singer = ?',
     //获得所有音乐
-    all: 'select a.music_name,a.singer,b.album_name from music a, album b where a.album_id = b.id limit ?,?',
+    all: 'select a.music_name,a.singer,a.music_img_url,b.album_name from music a, album b where a.album_id = b.id limit ?,?',
     //获得对应类型的音乐
-    type: 'select a.music_name,a.singer,b.album_name from music a, album b where a.type_id = (select id from type ' +
+    type: 'select a.music_name,a.singer,a.music_img_url,b.album_name from music a, album b where a.type_id = (select id from type ' +
       'where type_name = ?) and a.album_id = b.id limit ?,?',
     //保存音乐
     music_post: 'insert into music(album_id,type_id,music_name,singer,music_url,lyric_url,music_img_url) values(?,?,?,?,?,?,?)',
